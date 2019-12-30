3039d18ad175126673643883251761be3879c667
zohar_avr <zohar@bidalgo.com>
2019-12-30 10:37:37 +0200

    log work automatically

--- Content.js
+++ Content.js
@@ -1,9 +1,14 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log("from a content script:" + request.totalTime);
-        let els = document.getElementsByName('email');
-        els.forEach(el => {
-            el.setAttribute("placeholder", request.totalTime)
-            el.setAttribute("value", request.totalTime)
-        });
+        let optionsButton = document.getElementById('opsbar-operations_more');
+        optionsButton.click();
+        let logWork = document.getElementById('log-work');
+        logWork.click();
+        console.log('opened');
+        let el = document.getElementById('log-work-time-logged');
+        el.onload = function(){   let timeInHors = request.totalTime / 60;
+            el.setAttribute("value", timeInHors)};
     });
+
+      
